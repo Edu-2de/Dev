7a5fe33389476ea1a852d1a1db2047ff6367c1ef
Eduardo <edupaim1712@gmail.com>
2026-01-08 16:55:40 -0300

    Add Logavel trait with logging functionality

--- PHP/Projeto1/src/Logavel.php
+++ PHP/Projeto1/src/Logavel.php
@@ -1,3 +1,11 @@
 <?php
 
 namespace App\Financeiro;
+
+trait Logavel
+{
+    public function registrarLog($mensagem)
+    {
+        echo date('Y-m-d H:i:s') . " Log: $mensagem" . PHP_EOL;
+    }
+}
