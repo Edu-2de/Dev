32548db1c4df1d236a9aa19d73fd5489062582a3
Eduardo <edupaim1712@gmail.com>
2026-01-08 16:56:59 -0300

    Add ProjetoEspecial class with budget calculation method

--- PHP/Projeto1/src/ProjetoEspecial.php
+++ PHP/Projeto1/src/ProjetoEspecial.php
@@ -1,3 +1,11 @@
 <?php
 
 namespace App\Financeiro;
+
+class ProjetoEspecial extends Projeto
+{
+    public function calcularOrcamento(): float
+    {
+        return parent::calcularOrcamento() + 500;
+    }
+}
