9cdba66593070423509c54d97b9a0724a50f661e
Eduardo <edupaim1712@gmail.com>
2026-01-08 16:58:01 -0300

    Add Consultoria class implementing Orcavel interface for budget calculation

--- PHP/Projeto1/src/Consultoria.php
+++ PHP/Projeto1/src/Consultoria.php
@@ -1,3 +1,16 @@
 <?php
 
 namespace App\Financeiro;
+
+class Consultoria implements Orcavel
+{
+    public float $valor_fixo = 150.00;
+
+    use Logavel;
+
+    public function calcularOrcamento(): float
+    {
+        $this->registrarLog("calculando orçamento...");
+        return $this->valor_fixo;
+    }
+}
