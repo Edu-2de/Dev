cd628309449a06d35555471518d8bb4e8d9d919b
Eduardo <edupaim1712@gmail.com>
2026-01-08 16:59:59 -0300

    Add index.php file to initialize Financeiro namespace and include necessary classes

---
+++ PHP/Projeto1/index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Financeiro;
+
+require_once "src/Consultoria.php";
+require_once "src/Contrato.php";
+require_once "src/Formatador.php";
+require_once "src/Logavel.php";
+require_once "src/Orcavel.php";
+require_once "src/Projeto.php";
+require_once "src/ProjetoEspecial.php";
