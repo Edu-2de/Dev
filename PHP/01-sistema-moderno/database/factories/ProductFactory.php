fe2184ec3e9f72db59c7f9e5ff773ea3f1c0cce2
Eduardo <edupaim1712@gmail.com>
2026-01-13 14:45:19 -0300

    feat: Implement product listing with ShopController and Index.vue

--- PHP/01-sistema-moderno/database/factories/ProductFactory.php
+++ PHP/01-sistema-moderno/database/factories/ProductFactory.php
@@ -17,7 +17,10 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->words(3, true),
+            'description' => fake()->paragraph(),
+            'price' => fake()->randomFloat(2, 10, 5000),
+            'image_url' => 'https://placehold.co/600x400?text=' . urlencode($this->faker->word())
         ];
     }
 }
